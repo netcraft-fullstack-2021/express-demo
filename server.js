ea34b40c0e18be4be1a0845d36d7c579e07a3636
Ran <ran.wahle@gmail.com>
2022-02-28 10:22:16 +0200

    Using environment variables in bash

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 
 const app = express();
+const port = process.env.PORT
 
-app.listen(3000, () => console.log('I am listening'))
+app.listen(port, () => console.log('I am listening'))
