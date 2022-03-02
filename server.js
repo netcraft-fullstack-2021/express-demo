51cf0d5197852deaf065cce5d416cdaf257ed52d
Ran <ran.wahle@gmail.com>
2022-03-02 11:26:06 +0200

    A simple get endpoint

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ config();
 const app = express();
 const port = process.env.PORT;
 
+app.get('/', (req, res) => {
+    res.send('Hello from express');
+})
+
+
 app.listen(port, () => console.log(`I am listening on port ${port}`));
